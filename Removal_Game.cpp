8d243686922fb343302691b2d886797a3af752e9
Aayush Anand <159553781+AayushAnand39@users.noreply.github.com>
2025-05-13 14:29:24 +0530

    Add files via upload

---
+++ Removal_Game.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    long long sum = 0;
+    for (int i=0;i<n;i++) {
+        cin>>a[i];
+        sum+=a[i];
+    }
+    vector<vector<long long>> dp(n,vector<long long>(n));
+    for (int l=n-1;l>=0;l--) {
+        for (int r=l;r<n;r++) {
+            if (l==r) dp[l][r]=a[l];
+            else dp[l][r]=max(a[l]-dp[l+1][r],a[r]-dp[l][r-1]);
+        }
+    }
+    cout<<(sum+dp[0][n-1])/2<<endl;
+}
